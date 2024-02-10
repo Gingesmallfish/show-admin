a231f844dc6e8490bc72d6755aab4666b00b4341
姜小鱼 <1847535232@qq.com>
2024-02-10 16:50:52 +0800

    fest: 抽离 TailWindiCSS部分样式

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -13,26 +13,34 @@ const onSubmit = () => {
 </script>
 
 <template>
-  <el-row class="min-h-screen bg-indigo-500">
-    <el-col :lg="16" :md="12" class="flex items-center justify-center">
+  <el-row class="login-container">
+    <el-col :lg="16" :md="12" class="left">
       <div>
-        <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
-        <div class="text-gray-300 text-sm">此站点 《vue3 + vite实战商城项目》视频课程的演示地址，>点击立即学习</div>
+        <div>欢迎光临</div>
+        <div class="">此站点 《vue3 + vite实战商城项目》视频课程的演示地址，点击立即学习</div>
       </div>
     </el-col>
-    <el-col :lg="8" :md="12" class="bg-light-50 flex items-center justify-center flex-col">
-      <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
-      <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12" class="right">
+      <h2 class="title">欢迎回来</h2>
+      <div>
+        <span class="line"></span>
         <span>账号密码登录</span>
-        <span class="h-[1px] w-16 bg-gray-200"></span>
+        <span class="line"></span>
       </div>
       <el-form :model="form" class="w-[250px]">
-        <el-form-item >
-          <el-input v-model="form.username" placeholder="请输入用户名" />
+        <el-form-item>
+          <el-input v-model="form.username" placeholder="请输入用户名">
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
         </el-form-item>
-        <el-form-item >
-          <el-input v-model="form.password" placeholder="请输入密码"/>
+        <el-form-item>
+          <el-input v-model="form.password" placeholder="请输入密码">
+            <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" round color="#626aef" class="w-[250px]" @click="onSubmit">登 录</el-button>
@@ -42,4 +50,36 @@ const onSubmit = () => {
   </el-row>
 </template>
 
-<style scoped></style>
+<style scoped>
+.login-container{
+  @apply min-h-screen bg-indigo-500;
+}
+
+.login-container .left, .login-container .right{
+  @apply flex items-center justify-center;
+}
+
+.login-container .right {
+  @apply bg-light-50 flex-col;
+}
+
+.left > div > div:first-child {
+  @apply font-bold text-5xl text-light-50 mb-4;
+}
+
+.left > div > div:last-child {
+  @apply text-gray-300 text-sm
+}
+
+.right .title {
+  @apply font-bold text-3xl text-gray-800
+}
+
+.right > div {
+  @apply flex items-center justify-center my-5 text-gray-300 space-x-2
+}
+
+.right .line {
+  @apply h-[1px] w-16 bg-gray-200
+}
+</style>
